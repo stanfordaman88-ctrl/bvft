e6269fad4a73ebc536fa785d4eef823086c87c48
stanfordaman88-ctrl <stanfordaman88@gmail.com>
2026-07-10 17:20:14 +0100

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3000;
 const allowedOrigins = [
   'http://127.0.0.1:5000',
   'https://log-fram.netlify.app',
-  'https://hardone.netlify.app',
+  'https://billiejeann.netlify.app',
   'https://firmtrack.netlify.app',
   'https://bluestreet.netlify.app',
   'https://bigframe.netlify.app',
