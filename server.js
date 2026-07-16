b4007cc2df77758074d47c56b0ed4041c0bb4b16
stanfordaman88-ctrl <stanfordaman88@gmail.com>
2026-07-16 14:36:39 +0100

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3000;
 
 const allowedOrigins = [
   'http://127.0.0.1:5000',
-  'https://greatwind.netlify.app',
+  'https://windystorm.netlify.app',
   'https://firmtrack.netlify.app',
   'https://bluestreet.netlify.app',
   'https://bigframe.netlify.app',
