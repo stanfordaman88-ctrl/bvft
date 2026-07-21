9fb032b875a30c7b01b3f027d45270acfdbd539e
stanfordaman88-ctrl <stanfordaman88@gmail.com>
2026-07-21 13:59:25 +0100

    Update server.js

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const PORT = process.env.PORT || 3000;
 const allowedOrigins = [
   'http://127.0.0.1:5000',
   'https://mountrave.netlify.app',
+  'https://bigmover.netlify.app',
   'https://firmtrack.netlify.app',
   'https://bluestreet.netlify.app',
   'https://bigframe.netlify.app',
