1dc9d572c067a466ec580571e1b2d1a56f7ab21f
stanfordaman88-ctrl <stanfordaman88@gmail.com>
2026-08-14 23:42:46 +0100

    Update server.js

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const PORT = process.env.PORT || 3000;
 const allowedOrigins = [
   'http://127.0.0.1:5000',
   'https://skyfog.netlify.app',
+  'https://greatz.netlify.app',
   'https://headyed.netlify.app',
   'https://westgrove.netlify.app',
   'https://launchlan.netlify.app',
